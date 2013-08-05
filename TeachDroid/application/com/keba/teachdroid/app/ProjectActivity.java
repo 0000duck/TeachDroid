5e88ba3f51359e974097a092b78dfc53565d5a06
f481 <f481@LNZNBMHL.keba.co.at>
2013-08-05 09:38:48 +0200

    commit working?

---
+++ TeachDroid/application/com/keba/teachdroid/app/ProjectActivity.java
@@ -0,0 +1,25 @@
+package com.keba.teachdroid.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class ProjectActivity extends FragmentActivity {
+
+	
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_main);
+	// Create the adapter that will return a fragment for each of the three
+			// primary sections of the app.
+//			mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+			// Set up the ViewPager with the sections adapter.
+//			mViewPager = (ViewPager) findViewById(R.id.pager);
+//			mViewPager.setAdapter(mSectionsPagerAdapter);
+	
+	
+}
+
+}
