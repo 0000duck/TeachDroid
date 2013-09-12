9ee931278cff192bc4c9c289c1f2b89c4edd714c
fsd <fsd@LNZPC0104.keba.co.at>
2013-09-12 09:40:16 +0200

    as

---
+++ TeachDroid/application/com/keba/teachdroid/app/fragments/ProgramListFragment.java
@@ -0,0 +1,76 @@
+package com.keba.teachdroid.app.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+public class ProgramListFragment extends ListFragment {
+	/**
+	 * The fragment's current callback object, which is notified of list item
+	 * clicks.
+	 */
+	private SelectionCallback mCallbacks = sDummyCallbacks;
+	
+
+	/**
+	 * A dummy implementation of the {@link SelectionCallback} interface that
+	 * does nothing. Used only when this fragment is not attached to an
+	 * activity.
+	 */
+	private static SelectionCallback sDummyCallbacks = new SelectionCallback() {
+
+		public void onProgramSelected(int id) {
+		}
+	};
+
+	/**
+	 * A callback interface that all activities containing this fragment must
+	 * implement. This mechanism allows activities to be notified of item
+	 * selections.
+	 */
+	public interface SelectionCallback {
+		/**
+		 * Callback for when an item has been selected.
+		 */
+		public void onProgramSelected(int id);
+	}
+
+	public ProgramListFragment() {
+
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		// Activities containing this fragment must implement its callbacks.
+		if (!(activity instanceof SelectionCallback)) {
+			Log.w("ProjectListFragment", "Activity must implement fragment's callbacks.");
+			mCallbacks = sDummyCallbacks;
+		} else
+			mCallbacks = (SelectionCallback) activity;
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+
+		// Reset the active callbacks interface to the dummy implementation.
+		mCallbacks = sDummyCallbacks;
+	}
+
+	@Override
+	public void onListItemClick(ListView listView, View view, int position, long id) {
+		super.onListItemClick(listView, view, position, id);
+		mCallbacks.onProgramSelected(position);
+	}
+}
+
